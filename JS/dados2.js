eb8e166e657f963a23d7d4e9641a36223faf0011
CaiqueBNascimento <95309142+CaiqueBNascimento@users.noreply.github.com>
2022-02-10 10:33:50 -0300

    JS
    Learning Java Scripts

---
+++ JS/dados2.js
@@ -0,0 +1,12 @@
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto);
+let nome = "Caderno ";
+let categoria = "Papelaria";
+
+console.log(19.9 * 0.6);
+console.log(precoComDesconto);
+console.log("Produto: " + nome 
+    +  ", Categoria: " + categoria
+    +  ", Preco: " + preco
+    +  ", Desconto: "+ desconto);
